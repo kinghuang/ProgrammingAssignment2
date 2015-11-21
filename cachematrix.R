78c0ad7554d483c188d83c85157b8b8f1953b58f
King Chung Huang <kinghuang@mac.com>
2015-11-21 15:00:19 -0700

    Add brief comments in makeCacheMatrix
    Briefly describe the implementation of makeCacheMatrix. It is a simple
    object (list) containing get/set functions for a matrix and its inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,27 @@
 ## functions for getting and setting the matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+	# Initialize the inverse of x to NULL.
 	inv <- NULL
+	
+	# get and set functions for the matrix x.
+	# The set function also sets the inverse of x
+	# to NULL, to invalidate any previously cached
+	# inverse.
 	set <- function(y) {
 		x <<- y
 		inv <<- NULL
 	}
 	get <- function() x
+	
+	# get and set functions for the inverse of
+	# the matrix x.
 	setinv <- function(minv) inv <<- minv
 	getinv <- function() inv
+	
+	# Return a simple object (list) containing the
+	# get and set functions for a matrix and its
+	# inverse.
 	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
